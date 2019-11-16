531685541a4c6babe25a031f6879f19d76ee931c
Subak Systems <info@subak.jp>
2019-11-17 01:25:36 +0900

    2019年 11月17日 日曜日 01時25分36秒 JST

--- main/ui.ts
+++ main/ui.ts
@@ -1,4 +1,4 @@
-import {ui as main} from '../main/index'
+import {ui as main} from '../main'
 import {uiPort as port, ui as cuit} from '../'
 Object.assign(globalThis, {
   subject$: main(port, cuit, self)
