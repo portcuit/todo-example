790961de3cd675cbaf327f4545985a82e250b74c
Subak Systems <info@subak.jp>
2019-11-17 01:18:07 +0900

    2019年 11月17日 日曜日 01時18分07秒 JST

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 var __importStar = (this && this.__importStar) || function (mod) {
     if (mod && mod.__esModule) return mod;
     var result = {};
@@ -18,15 +7,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@pkit/core");
-var helper_1 = require("@pkit/helper");
-var worker = __importStar(require("@pkit/worker"));
-var ui = __importStar(require("./ui"));
-var action = __importStar(require("./action"));
-var port_1 = require("@pkit/core/port");
-var bridge = {
+const core_1 = require("@pkit/core");
+const helper_1 = require("@pkit/helper");
+const worker = __importStar(require("@pkit/worker"));
+const ui = __importStar(require("./ui"));
+const action = __importStar(require("./action"));
+const port_1 = require("@pkit/core/port");
+const bridge = {
     vnode: null,
-    ui: __assign(__assign({}, port_1.context), { reload: null }),
+    ui: Object.assign(Object.assign({}, port_1.context), { reload: null }),
     action: action.port
 };
 // exports.port = {
@@ -70,9 +59,7 @@ var bridge = {
 //       sink(port.quit),
 //       source(port.bridge.ui.terminated)))
 // };
-exports.uiPort = __assign(__assign({}, port_1.context), { worker: worker.port.child, ui: ui.port, bridge: bridge });
-var _ui = function (port, parent) {
-    return core_1.compose(worker.useChildWorker(port.worker, parent, port.bridge.vnode, port.bridge.ui.terminated), core_1.plug(helper_1.directSink, core_1.source(port.ui.view.vnode), core_1.sink(port.bridge.vnode)), core_1.plug(helper_1.directSink, core_1.source(port.bridge.ui.terminate), core_1.sink(port.bridge.ui.terminated)), action.ui(port.bridge.action, port.ui.state), ui.default(port, port.ui.state, port.ui.view, port.bridge.action));
-};
+exports.uiPort = Object.assign(Object.assign({}, port_1.context), { worker: worker.port.child, ui: ui.port, bridge });
+const _ui = (port, parent) => core_1.compose(worker.useChildWorker(port.worker, parent, port.bridge.vnode, port.bridge.ui.terminated), core_1.plug(helper_1.directSink, core_1.source(port.ui.view.vnode), core_1.sink(port.bridge.vnode)), core_1.plug(helper_1.directSink, core_1.source(port.bridge.ui.terminate), core_1.sink(port.bridge.ui.terminated)), action.ui(port.bridge.action, port.ui.state), ui.default(port, port.ui.state, port.ui.view, port.bridge.action));
 exports.ui = _ui;
 //# sourceMappingURL=index.js.map
--- main/window.ts
+++ main/window.ts
@@ -1,5 +1,5 @@
-import {default as main} from '../main'
-import {port, window as cuit} from '../'
-Object.assign(window, {
-  subject$: main(port, cuit, Worker)
-});
+// import {default as main} from '../main'
+// import {port, window as cuit} from '../index'
+// Object.assign(window, {
+//   subject$: main(port, cuit, Worker)
+// });
