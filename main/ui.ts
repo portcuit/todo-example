0ba1af882e28be27a7f04fb7afed053aff2acf5d
Subak Systems <info@subak.jp>
2019-11-17 11:55:27 +0900

    2019年 11月17日 日曜日 11時55分27秒 JST

--- main/ui.ts
+++ main/ui.ts
@@ -1,5 +1,7 @@
+import {EventEmitter} from 'events'
 import {ui as main} from '../main'
 import {uiPort as port, ui as cuit} from '../'
 Object.assign(globalThis, {
   subject$: main(port, cuit, self)
 });
+new EventEmitter;
