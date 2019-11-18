1281233d6023ee8e492749a26d6f3fb5db194427
Subak Systems <info@subak.jp>
2019-11-18 16:58:25 +0900

    2019年 11月18日 月曜日 16時58分25秒 JST

--- src/electron/port.js
+++
@@ -1,8 +0,0 @@
-const {prepare} = require('@pkit/core')
-
-module.exports = prepare({
-  context: {
-    main: require('@pkit/core/port/context').raw,
-  },
-  electron: require('@pkit/electron/port').raw
-})
