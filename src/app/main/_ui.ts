ab271f001105eee76fe3542bd45c4d5bd0183fab
Subak Systems <info@subak.jp>
2019-11-18 17:24:27 +0900

    rm src/app

--- src/app/main/_ui.ts
+++
@@ -1,5 +0,0 @@
-import {ui as main} from '../main'
-import {uiPort as port, uiKit as cuit} from '../'
-Object.assign(globalThis, {
-  subject$: main(port, cuit, self)
-});
--- src/app/main/window.ts
+++
@@ -1,5 +0,0 @@
-import {default as main} from '../main'
-import {port, window as cuit} from '../index'
-Object.assign(window, {
-  subject$: main(port, cuit, Worker)
-});
